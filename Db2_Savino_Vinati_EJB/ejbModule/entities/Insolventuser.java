9c135e4123e8405e6fe38a6390e0f42236d0708a
Matteo Savino <matteosavino98@gmail.com>
2021-11-29 15:53:05 +0100

    Version 0.0.8
    Jpa entities: wip

---
+++ Db2_Savino_Vinati_EJB/ejbModule/entities/Insolventuser.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the insolventuser database table.
+ * 
+ */
+@Entity
+@Table(name = "insolventuser", schema="db2_savino_vinati")
+@NamedQuery(name="Insolventuser.findAll", query="SELECT s FROM Serviceinternet s")
+public class Insolventuser implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+	
+	private int failedpaymentcount;
+
+	private boolean insolvent;
+
+	public Insolventuser() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getFailedpaymentcount() {
+		return failedpaymentcount;
+	}
+
+	public void setFailedpaymentcount(int failedpaymentcount) {
+		this.failedpaymentcount = failedpaymentcount;
+	}
+
+	public boolean isInsolvent() {
+		return insolvent;
+	}
+
+	public void setInsolvent(boolean insolvent) {
+		this.insolvent = insolvent;
+	}
+
+}

037f492cb4476557db581d32f03ee137f64cfde1
Matteo Savino <matteosavino98@gmail.com>
2021-12-27 16:03:06 +0100

    Version 0.0.123
    WIP

--- Db2_Savino_Vinati_EJB/ejbModule/services/SalesReportService.java
+++ Db2_Savino_Vinati_EJB/ejbModule/services/SalesReportService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 
 import entities.MvAlert;
 import entities.MvBestproduct;
@@ -13,7 +12,6 @@ import entities.MvInsolventUser;
 import entities.MvPackage;
 import entities.MvPackageperiod;
 import entities.MvSuspendedorder;
-import exceptions.BadSalesReportDataRetrieval;
 
 @Stateless
 public class SalesReportService {
